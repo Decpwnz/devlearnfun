d804854927961b6e6c970ee3dbb6caa8340c4c49
Marijus <electromenn@gmail.com>
2024-11-05 09:56:12 +0200

    points

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-// Write function RemoveExclamationMarks which
-// removes all exclamation marks from a given string.
+// Our football team has finished the championship.
 
-function removeExclamationMarks(s) {
-  return s.split('!').join('');
+// Our team's match results are recorded in a collection of strings.
+// Each match is represented by a string in the format "x:y",
+// where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection
+// and returns the number of points our team (x) got in the
+// championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+  let x,
+    y,
+    points = 0;
+
+  for (let i = 0; i < games.length; i++) {
+    x = games[i][0];
+    y = games[i][2];
+
+    if (x > y) points += 3;
+    if (x < y) points += 0;
+    if (x === y) points += 1;
+  }
+  return points;
 }
 
-console.log(removeExclamationMarks('Hello!!!'));
+console.log(
+  points(['1:0', '2:0', '3:0', '4:0', '2:1', '3:1', '4:1', '3:2', '4:2', '4:3'])
+);
